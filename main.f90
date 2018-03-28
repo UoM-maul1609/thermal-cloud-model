78a831b51e0d6fad82f1b7f5298176545ef5ce95
Paul Connolly <p.connolly@manchester.ac.uk>
2018-03-28 09:05:52 +0100

    added capability to do normal random sampling

--- main.f90
+++ main.f90
@@ -20,6 +20,7 @@
 	
     program main
         use nrtype1
+        use random, only : random_normal
         use sub, only : n_mode, n_sv, giant_flag, method_flag, sv_flag, &
         		act_frac2, &
         		n_aer, d_aer, sig_aer, molw_core, density_core, nu_core, org_content, &
@@ -33,6 +34,13 @@
         real(sp) :: w1,t1,p1,act_frac1
         
         character (len=200) :: nmlfile = ' '
+        
+		! for random number:
+		real(sp) :: r, mean_w, sigma_w
+		real(sp), dimension(10,10) :: rs
+		integer(i4b), allocatable, dimension(:) :: seed
+		integer(i4b) :: l, i, n_rand
+		logical :: rand_dist=.false.
 
 
 
@@ -47,7 +55,8 @@
         namelist /bulk_aerosol_spec/ n_aer, d_aer, sig_aer, molw_core, density_core, &
         					nu_core, org_content, molw_org, density_org, &
         					delta_h_vap, &
-        					nu_org, log_c_star, p_test, t_test, w_test
+        					nu_org, log_c_star, p_test, t_test, w_test, &
+        					rand_dist, n_rand, mean_w,sigma_w
         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
@@ -81,15 +90,55 @@
         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         ! call activation code												   !
         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        call ctmm_activation(n_mode,n_sv,sv_flag, &
-        					n_aer, d_aer,sig_aer,molw_core, &
-                            density_core, nu_core, &
-                            org_content,molw_org, density_org, delta_h_vap, nu_org,  &
-                            log_c_star, &
-                            w_test, t_test,p_test, &
-                            act_frac)
-        print *,'act_frac1, n_act',act_frac,  sum(act_frac*n_aer)
+        if(.not. rand_dist) then
+        	print *,'act_frac1, n_act'
+			call ctmm_activation(n_mode,n_sv,sv_flag, &
+								n_aer, d_aer,sig_aer,molw_core, &
+								density_core, nu_core, &
+								org_content,molw_org, density_org, delta_h_vap, nu_org,  &
+								log_c_star, &
+								w_test, t_test,p_test, &
+								act_frac)
+			print *,act_frac,  sum(act_frac*n_aer)
+		else
+			! random number generator
+			call random_seed(size=l)
+			allocate(seed(1:l))
+			seed(:)=2
+			call random_seed(put=seed)
+			
+			
+
+			!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			! call activation code for n_rand samples							   !
+			!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			print *,'w,act_frac1, n_act'
+			do i=1,n_rand
+				r=random_normal() ! from the Netlib
+				r=r*sigma_w
+				r=r+mean_w
+				r=abs(r) ! only positive values
+				w_test=r
+				!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+				! initialise arrays								                   !
+				!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+				call initialise_arrays(n_mode,p_test,t_test,w_test)
+				!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+				
+				call ctmm_activation(n_mode,n_sv,sv_flag, &
+									n_aer, d_aer,sig_aer,molw_core, &
+									density_core, nu_core, &
+									org_content,molw_org, density_org, delta_h_vap, nu_org,  &
+									log_c_star, &
+									w_test, t_test,p_test, &
+									act_frac)
+				print *,w_test,act_frac,  sum(act_frac*n_aer)
+			enddo
+			deallocate(seed)
+			!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		endif
         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
+		
     end program main
