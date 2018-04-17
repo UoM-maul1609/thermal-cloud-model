7bb850fe6c4bc343651767e85230ba588006e791
Paul Connolly <p.connolly@manchester.ac.uk>
2018-04-18 00:43:33 +0100

    variables for new warm 2-moment scheme added

--- variables.f90
+++ variables.f90
@@ -60,15 +60,17 @@
         logical :: monotone=.true.,theta_flag=.false., &
         			hm_flag=.true.
 		integer(i4b) :: advection_scheme=0,microphysics_flag=0
+		character (len=200) :: bam_nmlfile = ' '
 
         ! variables for model
         real(sp), allocatable,dimension(:,:) :: q_read ! nq x nlevels_r
         real(sp), dimension(nlevels_r) :: theta_read,rh_read, &
                   z_read
         real(sp) :: dx, dz,dt, runtime, psurf, theta_surf,tsurf, t_cbase, t_ctop, t_thresh, &
-        			t_thresh2, w_cb, theta_q_sat,t1old, p111, num_ice, mass_ice
+        			t_thresh2, w_cb, theta_q_sat,t1old, p111, num_ice, mass_ice, &
+        			num_drop
         integer(i4b) :: ip,kp, n_levels_s, ord, o_halo,halo, updraft_type
-        logical :: ice_init=.true.
+        logical :: ice_init=.true., drop_num_init=.false.
         integer(i4b) :: nq = 9, qv=1,qc=2,qr=3,nqc=-1,nqr=-1,qs=4,qg=5,qi=6, &
                                    nqi=7,nqs=8,nqg=9, nprec=4
         
